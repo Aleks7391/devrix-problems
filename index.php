cb5a723609686b7160e82f8d5ef7583747b2ccaf
Aleks7391 <aleks.ganev7@gmail.com>
2021-05-18 14:56:07 +0300

    commit

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ if ( have_posts() ) {
 
 	// Previous/next page navigation.
 	?>
-	<div class="nav-previous alignleft"><?php previous_posts_link( 'Older posts' ); ?></div>
-
+	<div class="nav-previous alignleft"><?php previous_posts_link( 'Newer posts' ); ?></div>
+	<div class="nav-next alignright"><?php next_posts_link( 'Older posts' ); ?></div>
 <?php
 } else {
 
